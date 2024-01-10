84d6b92bf132d6cf91873f70b34a19285836be0e
SistemasRobóticos-FIEC <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2024-01-09 23:16:27 -0500

    Update clonarObjetos.py

--- clonarObjetos.py
+++ clonarObjetos.py
@@ -37,8 +37,13 @@ def box_calc(size_xyz, pallet_xyz):
                 xyz_list = xyz_list + [[(i+0.5)*size_x, (j+0.5)*size_y, (h+0.5)*size_z]]
     return xyz_list
 
-possis = mbox("Ingrese distancia entre un objeto y otro",entry=[100,100,130])
-cantid = mbox("Ingrese cuando clone desea en X, Y y Z",entry=[5,3,2])
+tam = [100,100,130]
+num = [3,2,2]
+possis = InputDialog("Ingrese distancia entre un objeto y otro",title="objetos disponibles",
+                     value=tam)
+#possis = [float(x.replace(' ','')) for x in possis.split(',')]
+cantid = InputDialog("Ingrese cuando clone desea en X, Y y Z",value=num,title="ingrese el arreglo de objetos")
+#cantid = [float(x.replace(' ','')) for x in cantid.split(',')]
 posinit = box_calc([int(possis[0]),int(possis[1]),int(possis[2])],
                    [int(cantid[0]),int(cantid[1]),int(cantid[2])])
 
@@ -59,4 +64,3 @@ def rr(inf , may):
 
 for i in range(len(posinit)):
     RDK.Item("bot"+str(i)).Recolor([rr(0,1),rr(0,1),rr(0,1),1])
-
